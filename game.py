f16bcbbd4be7a7290a81e1b08c4eb0ad53980e4d
Gil Gonçalves <lursty@gmail.com>
2018-10-20 11:05:12 +0100

    Part 3 is done
    Lots of int(), I need to find a better way of doing it

--- game.py
+++ game.py
@@ -3,8 +3,129 @@ import libtcodpy as libtcod
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
-playerx = int(SCREEN_WIDTH/2)
-playery = int(SCREEN_HEIGHT/2)
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
+
+class Rect:
+    #a rectangle on the map. used to characterize a room.
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
+def create_h_tunnel(x1, x2, y):
+    x1 = int(x1)
+    x2 = int(x2)
+    y = int(y)
+
+    global map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+
+def create_v_tunnel(y1, y2, x):
+    y1 = int(y1)
+    y2 = int(y2)
+    x = int(x)
+
+    global map
+    #vertical tunnel
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+def create_room(room):
+    global map
+    #go through the tiles in the rectangle and make them passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+
+
+def make_map():
+    global map
+
+    #fill map with "blocked" tiles
+    map = [[ Tile(True)
+        for y in range(MAP_HEIGHT) ]
+            for x in range(MAP_WIDTH) ]
+
+
+    rooms = []
+    num_rooms = 0
+
+    for r in range(MAX_ROOMS):
+        #random width and height
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        #random position without going out of the boundaries of the map
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
+
+        #"Rect" class makes rectangles easier to work with
+        new_room = Rect(x, y, w, h)
+
+        #run through the other rooms and see if they intersect with this one
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+
+        if not failed:
+            #this means there are no intersections, so this room is valid
+
+            #"paint" it to the map's tiles
+            create_room(new_room)
+
+            #center coordinates of new room, will be useful later
+            (new_x, new_y) = new_room.center()
+
+            #optional: print "room number" to see how the map drawing worked
+            #          we may have more than ten rooms, so print 'A' for the first room, 'B' for the next...
+            # room_no = Object(int(new_x), int(new_y), chr(65+num_rooms), libtcod.white)
+            # objects.insert(0, room_no) #draw early, so monsters are drawn on top
+
+            if num_rooms == 0:
+                #this is the first room, where the player starts at
+                player.x = int(new_x)
+                player.y = int(new_y)
+
+            else:
+                #all rooms after the first:
+                #connect it to the previous room with a tunnel
+
+                #center coordinates of previous room
+                (prev_x, prev_y) = rooms[num_rooms-1].center()
+
+                #draw a coin (random number that is either 0 or 1)
+                if libtcod.random_get_int(0, 0, 1) == 1:
+                    #first move horizontally, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+                else:
+                    #first move vertically, then horizontally
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+
+            #finally, append the new room to the list
+            rooms.append(new_room)
+            num_rooms += 1
 
 
 def handle_keys():
@@ -76,20 +197,6 @@ class Tile:
         if block_sight is None: block_sight = blocked
         self.block_sight = block_sight
 
-def make_map():
-    global map
-
-    #fill map with "unblocked" tiles
-    map = [[ Tile(False)
-        for y in range(MAP_HEIGHT) ]
-            for x in range(MAP_WIDTH) ]
-
-    map[30][22].blocked = True
-    map[30][22].block_sight = True
-    map[50][22].blocked = True
-    map[50][22].block_sight = True
-
-
 def render_all():
     global color_light_wall
     global color_light_ground
@@ -123,6 +230,9 @@ con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 #create object representing the player
 player = Object(int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2), '@', libtcod.white)
+
+player.x = 25
+player.y = 23
 
 #create an NPC
 npc = Object(int(SCREEN_WIDTH/2) - 5, int(SCREEN_HEIGHT/2), '@', libtcod.yellow)
